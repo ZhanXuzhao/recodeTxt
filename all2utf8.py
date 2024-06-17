fad52133f2d15cc992114c92669c255dc20b6be9
zhanxuzhao <649912323@qq.com>
2024-06-17 12:30:08 +0800

    # 注意路径\需要手工改为\\

--- all2utf8.py
+++ all2utf8.py
@@ -1,6 +1,7 @@
 from sc.textfile import txtfile
 
 if __name__ == '__main__':
+    # 注意路径\需要手工改为\\
     yourpath = "D:\\Projects\\GHY\语料\\01先秦"
     file_ext='.txt|.csv'
     txtfile.allpath_txt_encoding_to_utf8(yourpath, file_ext)
